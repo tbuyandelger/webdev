211708ed23858f494ab816e10a9bc6bb5627936d
tbuyandelger <tbuyandelger.mn@gmail.com>
2020-03-17 08:05:25 -0700

    JS код бичив

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,9 @@
-
+//Кельвиний утгыг хадгалах хувьсагч
+const kelvin = 295;
+console.log(`Кельвин ${kelvin} К`);
+//Цельсийн утгыг хадгалах хувьсагч
+let celsius = kelvin - 273;
+console.log(`Цельсийн градус: ${celsius} С`);
+//Фаренгейтийн утгыг хадгалах хувьсагч
+let fahrenheit = celsius * (9 / 5) + 32;
+console.log(`Фаренгейтийн градус: ${Math.floor(fahrenheit)} F`);
